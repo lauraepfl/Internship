413661a23c8b3294bace106a634221d4334999bb
lauraepfl <laura.garciaadanez@epfl.ch>
2025-02-17 14:24:42 +0100

    new test in 2d

--- getusedtocode_tests/test_2d/representation2d.py
+++ getusedtocode_tests/test_2d/representation2d.py
@@ -1,10 +1,11 @@
 import yt
 # Load a dataset
 ds = yt.load('output_00001/info_00001.txt')
+print(ds)
 # This is to make a slice plot and a projection plot
-p = yt.SlicePlot(ds, 'z', 'density')
-p.save()  # save it as a png
+#p = yt.SlicePlot(ds, 'z', 'density')
+#p.save()  # save it as a png
 
 # Do a projection plot (actually integrate over the l.o.s.)
-p = yt.ProjectionPlot(ds, 'z', 'density', weight_field='ones')
-p.save()  # save it as a png
+#p = yt.ProjectionPlot(ds, 'z', 'density', weight_field='ones')
+#p.save()  # save it as a png
